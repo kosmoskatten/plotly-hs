c7ea72eae1ccd2ccf70ca9230dbbe47f3ca08835
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-03-11 10:38:36 +0100

    GOP simulation demo.

--- plotly-hbar-demo/src/Main.hs
+++ plotly-hbar-demo/src/Main.hs
@@ -1,10 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text)
+import System.Environment (getArgs)
+import System.Random (randomRIO)
+
 import HBar
 
 main :: IO ()
-main = runFruitPopulation
+main = do
+  selection <- head <$> getArgs
+  case selection of
+    "fruit" -> runFruitPopulation
+    "gop"   -> runGopPrimarySimulation
+    _       -> print ("fruit or gop" :: String)
 
 runFruitPopulation :: IO ()
 runFruitPopulation =
@@ -27,3 +39,38 @@ fruitPopulation = do
   addItemWithCategory "Banana" "Denmark" 8
 
   commit
+
+runGopPrimarySimulation :: IO ()
+runGopPrimarySimulation =
+  runHBar "http://localhost:8888" "GOP primary poll's simulation"
+          gopPrimarySimulation
+
+gopPrimarySimulation :: HBar ()
+gopPrimarySimulation =
+  forever $ do
+    state     <- selectFrom states
+    candidate <- selectFrom candidates
+    vote      <- fromIntegral <$> selectFrom votes
+
+    addItemWithCategory candidate state vote
+    commit
+    wait
+
+selectFrom :: [a] -> HBar a
+selectFrom xs = do
+  ind <- liftIO $ randomRIO (0, length xs - 1)
+  return $ xs !! ind
+
+states :: [Text]
+states = [ "Arizona", "Ohio", "Oregon", "Florida", "Texas"
+         , "Nevada", "North Carolina", "Washington" ]
+
+candidates :: [Text]
+candidates = [ "Trump", "Cruz", "Kasich", "Fiorina"
+             , "Rubio", "Bush", "Carson" ]
+
+votes :: [Int]
+votes = [1..25]
+
+wait :: HBar ()
+wait = liftIO $ threadDelay 1000000
